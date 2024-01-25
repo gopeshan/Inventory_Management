48bc26669ce911a834f4d74e1b9da22c36bbfb99
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-01-24 23:02:04 -0500

    Update product.py
    Set all variables and prints all information regarding a product.

--- product.py
+++ product.py
@@ -1,1 +1,9 @@
+class Product:
+    def __init__(self, id, name, price, quantity):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
+    def __str__(self):
+        return f"Product ID: {self.id}, Name: {self.name}, Price: ${self.price:.2f}, Quantity: {self.quantity}"

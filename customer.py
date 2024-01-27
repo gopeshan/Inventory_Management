bd5ab90244f45d37639e7b374eaff3d44fb79ea4
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-01-26 19:10:39 -0500

    Update customer.py
    Added print statement for customer ID.

--- customer.py
+++ customer.py
@@ -4,4 +4,5 @@ class Customer:
         self.name = name
         self.email = email
 
-  def __str__(self):
+    def __str__(self):
+        return f"Customer ID: {self.id}, Name: {self.name}, Email: {self.email}"

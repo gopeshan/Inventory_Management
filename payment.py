73b7319c458b8779452ea9eae5111f91da15c5d5
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-01-31 01:26:46 -0500

    Update payment.py
    Added variables to hold and return statement to display credit/debit card information.

--- payment.py
+++ payment.py
@@ -1,1 +1,9 @@
 class CreditDebitCard:
+    def __init__(self, card_number, card_holder, expiration_date, cvv):
+        self.card_number = card_number
+        self.card_holder = card_holder
+        self.expiration_date = expiration_date
+        self.cvv = cvv
+
+    def __str__(self):
+        return f"Card Number: {self.card_number}, Card Holder: {self.card_holder}, Expiration Date: {self.expiration_date}, CVV: {self.cvv}"
